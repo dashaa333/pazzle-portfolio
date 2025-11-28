b49aabcbc8e93498aaeb830968e248141bf6419b
Daria <pphzsf7vpt@privaterelay.appleid.com>
2025-11-28 20:31:48 +0500

    Add files via upload

---
+++ course-1/practices/25.11/25.11/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 12;
+int b = 5;
+int sum = a + b;
+Console.WriteLine(sum);
+int diff = a - b;
+Console.WriteLine(diff);
+int prod = a * b;
+Console.WriteLine(prod);
+int div = a / b;
+Console.WriteLine(div);
+int mod = a % b;
+Console.WriteLine(mod);
+
+string name = Console.ReadLine();
+name = "Даша";
+Console.WriteLine("Привет, " + name + "!");
+
